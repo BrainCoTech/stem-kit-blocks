6c2fa8487cc73411b478babc1e899e08c13958a3
Dongsheng Sun <sundong95125@gmail.com>
2019-10-02 14:46:57 -0400

    refactored flex hand and transmitter code

--- arduino/FlexSensorTransmitter/FlexSensorTransmitter.ino
+++ arduino/FlexSensorTransmitter/FlexSensorTransmitter.ino
@@ -1,56 +1,48 @@
-  //Transmitter Code (Glove) - Mert Arduino and Tech
-
+//Transmitter Code (Glove) - Mert Arduino and Tech
 #include <SPI.h>                      //the communication interface with the modem
 #include "RF24.h"                     //the library which helps us to control the radio modem
 
-int msg[5]; //Total number of data to be sent (data package)
+#define CE_PIN 9
+#define CSN_PIN 10
+#define SERIAL_PORT 9600
+#define FINGER_COUNT 5
 
-//define the flex sensor input pins
-int flex_5 = A5;
-int flex_4 = A4;
-int flex_3 = A3;
-int flex_2 = A2;
-int flex_1 = A1;
-
-//define variables for flex sensor values
-int flex_5_val;
-int flex_4_val;
-int flex_3_val;
-int flex_2_val;
-int flex_1_val;
-
-RF24 radio(9,10);                     //9 and 10 are a digital pin numbers to which signals CE and CSN are connected.
-                                      
+RF24 radio(CE_PIN, CSN_PIN);                     //9 and 10 are a digital pin numbers to which signals CE and CSN are connected.
 const uint64_t pipe = 0xE0E0F0F1E1LL; //the address of the modem, that will receive data from Arduino.
 
+//define the flex sensor input pins
+typedef enum {
+    THUMB = 0,
+    INDEX = 1,
+    MIDDLE = 2,
+    RING = 3,
+    PINKY = 4
+} Finger;
+
+static int flex_sensor_analog_pins = {A1, A2, A3, A4, A5};
+static int flex_upper_bounds = {575, 590, 600, 600, 580};
+static int flex_lower_bounds = {530, 520, 520, 520, 510};
+static int servo_min_degrees = {5, 180, 5, 5, 180};
+static int servo_max_degrees = {150, 35, 150, 150, 35};
+
+static int radio_msg[FINGER_COUNT]; //Total number of data to be sent (data package)                         
+
+void mappedFlexValue(int finger) {
+  int finger_flex_value = analogRead(flex_sensor_analog_pins[finger]);
+  radio_msg[finger] = map(finger_flex_value, flex_lower_bounds[finger], flex_upper_bounds[finger], servo_min_degrees[finger], servo_max_degrees[finger]);
+}
 
 void setup(void){
-  Serial.begin(9600);
+  Serial.begin(SERIAL_PORT);
   radio.begin();                      //it activates the modem.
   radio.openWritingPipe(pipe);        //sets the address of the receiver to which the program will send data.
 }
 
 void loop(void){
 
-  flex_1_val = analogRead(flex_1); 
-  flex_1_val = map(flex_1_val, 530, 575, 5, 150);
+  for (int i = 0; i < FINGER_COUNT; i++) {
+     mappedFlexValue((Finger)i);
+  }
   
-  flex_2_val = analogRead(flex_2);
-  flex_2_val = map(flex_2_val, 520, 590, 180, 35);
- 
-  flex_3_val = analogRead(flex_3);
-  flex_3_val = map(flex_3_val, 520, 600, 5, 150);
- 
-  flex_4_val = analogRead(flex_4);
-  flex_4_val = map(flex_4_val, 520, 600, 5, 150);
-  
-  flex_5_val = analogRead(flex_5);
-  flex_5_val = map(flex_5_val, 510, 580, 180, 35);
-  
-  msg[4] = flex_5_val;
-  msg[3] = flex_4_val;
-  msg[2] = flex_3_val;
-  msg[1] = flex_2_val;
-  msg[0] = flex_1_val;
-  radio.write(msg, sizeof(msg));
+  radio.write(radio_msg, sizeof(radio_msg));
 }
